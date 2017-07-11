69662aeed7c01265f506a5f57f828bcd0f481fbb
Kavilan Nair <kavilann@gmail.com>
2017-07-11 11:31:09 +0200

    Initial Commit

---
+++ CryptoTrackr/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  CryptoTrackr
+//
+//  Created by Kavilan Nair on 2017/07/11.
+//  Copyright © 2017 Kavilan Nair. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
